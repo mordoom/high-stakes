4b4c280671412aa445d53d7eff9becebda98f35c
Alex Mordue <alex.mordue@hotmail.com.au>
2019-11-13 22:55:45 +1100

    initial commit

---
+++ Assets/Scripts/VampireController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VampireController : MonoBehaviour {
+    public int health = 3;
+    public bool dead = false;
+
+    void Start () {
+
+    }
+
+    void Update () {
+
+    }
+
+    internal void Hurt () {
+        if (dead) {
+            return;
+        }
+
+        health--;
+
+        if (health <= 0) {
+            Die ();
+        }
+    }
+
+    private void Die () {
+        Debug.Log ("dead");
+        dead = true;
+    }
+}
